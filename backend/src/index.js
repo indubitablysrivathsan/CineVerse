a7448f9f3dc67c09ebdbd8ecd83d7858a580c88c
indubitablysrivathsan <srivathsan.g.21@gmail.com>
2026-02-12 17:01:23 +0530

    bug: fixing cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 4000;
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://your-vercel-domain.vercel.app"
+    "https://cine-verse-lovat.vercel.app"
   ],
   credentials: true
 }));
